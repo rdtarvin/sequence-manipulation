ec4feaada7fcf274c9248949ea5edfafc3f52994
rdtarvin <rdtarvin@gmail.com>
2014-04-16 16:11:34 -0500

    Create clean_seq_names.py
    This script cleans the sequence names and descriptions in a fasta file.

---
+++ clean_seq_names.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 14 20:27:07 2014
+
+@author: RDT
+"""
+
+'''This script reorganizes the rec.id and rec.description of a fasta file.'''
+
+from Bio import SeqIO
+import sys
+
+def clean_seqs(infile,genename,database):
+    '''Open fasta infile and return iterator of SeqRecords with protein sequences.'''
+    records = SeqIO.parse(infile, 'fasta')
+    blasthit=str(infile[:-6])
+    newrecords=[]
+#    print filename
+    for rec in records:
+        items=(rec.description).split(' ') # turns description into a list
+#        print items
+        rec.id = genename+items[1] # adds gene name to sequence ID
+        newitem=''
+#        print length
+        for i in range(3,len(items)):
+            newitem='%s %s ' %(newitem,items[i]) # concatenates paths
+        if items[1][0] == '_':
+            items[1] = items[1][1:]
+        rec.description="'%s' %s %s %s %s" %(blasthit, database, items[1], items[2], newitem) # rewrites description
+#        print rec.id
+#        print rec.description
+        newrecords.append(rec)
+    outfile=genename+'_clean.fasta'
+    with open(outfile,'w') as f:
+        SeqIO.write(newrecords, f, 'fasta')    
+
+if __name__ == '__main__':
+    infile = sys.argv[1]
+    gene = sys.argv[2]
+    database= sys.argv[3]
+    clean_seqs(infile,gene,database)
